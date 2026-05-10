5769f54e8db674b3b14ef532702ac1491d388966
TwCory <voltzbigtime@gmail.com>
2026-05-09 22:17:51 -0400

    CLI stuff

---
+++ include/nbapi/voltos/cli/prompt.h
@@ -0,0 +1,33 @@
+#ifndef _NBAPI_VOLTOS_CLI_PROMPT_H_
+#define _NBAPI_VOLTOS_CLI_PROMPT_H_
+
+#define CLI_PROMPT_CLASS_MAP			"(config-class-map)# "
+#define CLI_PROMPT_CONFIGURE			"(config)# "
+
+#define CLI_PROMPT_DHCP_POOL			"(config-dhcp)# "
+#define CLI_PROMPT_DHCPV6_POOL			"(config-dhcpv6)# "
+
+#define CLI_PROMPT_INTERFACE			"(config-if)# "
+#define CLI_PROMPT_INTERFACE_RANGE		"(config-if-range)# "
+#define CLI_PROMPT_IP_EXT_NACL			"(config-ext-nacl)# "
+#define CLI_PROMPT_IP_STD_NACL
+#define CLI_PROMPT_IPC_ZONE			"(config-ipc-zone)# "
+
+#define CLI_PROMPT_L2TP_CLASS			"(config-l2tp-class)# "
+#define CLI_PROMPT_LINE				"(config-line)# "
+
+#define CLI_PROMPT_ROUTE_MAP			"(config-route-map)# "
+#define CLI_PROMPT_ROUTE_TAG_LIST		"(config-route-tag-list)# "
+#define CLI_PROMPT_ROUTER			"(config-router)# "
+#define CLI_PROMPT_ROUTER_AF			"(config-router-af)# "
+
+#define CLI_PROMPT_SUBINTERFACE			"(config-subif)# "
+
+#define CLI_PROMPT_VLAN				"(config-vlan)# "
+#define CLI_PROMPT_VRF				"(config-vrf)# "
+#define CLI_PROMPT_VRF_AF			"(config-vrf-af)# "
+#define CLI_PROMPT_VRF_AF_TOPOLOGY		"(config-vrf-af-topology)# "
+
+#define CLI_PROMPT_X25_HUNT_GROUP		"(config-x25-huntgroup)# "
+
+#endif /* _NBAPI_VOLTOS_CLI_PROMPT_H_ */
