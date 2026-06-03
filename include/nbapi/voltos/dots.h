a595d02b8f5de70d21a2b76fad407f47fb65716c
Cory <70068963+TwCory@users.noreply.github.com>
2026-06-03 11:28:16 -0400

    test

--- include/nbapi/voltos/dots.h
+++ include/nbapi/voltos/dots.h
@@ -1,4 +1,14 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+/*
+ *  Distributed Denial-of-Service Open
+ *  Threat Signaling (DOTS) Signal
+ *  Channel
+ *
+ */
+
 #ifndef _NBAPI_VOLTOS_DOTS_H_
 #define _NBAPI_VOLTOS_DOTS_H_
+
 
 #endif /* _NBAPI_VOLTOS_DOTS_H_ */
