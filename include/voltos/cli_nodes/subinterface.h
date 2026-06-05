cbc87032959dd93183258b84eaafe0f67d29f056
Cory <70068963+TwCory@users.noreply.github.com>
2026-06-05 15:05:29 -0400

    Create subinterface.h

---
+++ include/voltos/cli_nodes/subinterface.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+#ifndef VOLTOS_CLI_NODES_SUBINTERFACE_H
+#define VOLTOS_CLI_NODES_SUBINTERFACE_H
+
+#define __foreach_cli_subinterface_node \
+  _ ("encapsulation", "") \
+
+#endif /* VOLTOS_CLI_NODES_SUBINTERFACE_H */
