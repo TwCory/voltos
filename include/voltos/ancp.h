836e8fea3ba69fa354fb21d224e752f75486d93a
Cory <70068963+TwCory@users.noreply.github.com>
2026-06-08 15:13:47 -0400

    Minor updates, ANCP implementation

--- include/voltos/ancp.h
+++ include/voltos/ancp.h
@@ -1,6 +1,13 @@
-#ifndef _VOLTOS_ANCP_H_
-#define _VOLTOS_ANCP_H_
+// SPDX-License-Identifier: BSD-3-Clause
+
+/*
+ *  Access Node Control Protocol (ANCP)
+ *
+ */
+
+#ifndef VOLTOS_ANCP_H
+#define VOLTOS_ANCP_H
 
 #include <nbapi/voltos/ancp.h>
 
-#endif /* _VOLTOS_ANCP_H_ */
+#endif /* VOLTOS_ANCP_H */
